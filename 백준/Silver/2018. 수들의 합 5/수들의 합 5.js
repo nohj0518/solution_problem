014b8a431dfe8933e6b1eee527319a17ba5bad21
노현주 <38433755+nohj0518@users.noreply.github.com>
2024-09-12 23:23:08 +0900

    [Silver V] Title: 수들의 합 5, Time: 212 ms, Memory: 9600 KB -BaekjoonHub

--- 백준/Silver/2018. 수들의 합 5/수들의 합 5.js
+++ 백준/Silver/2018. 수들의 합 5/수들의 합 5.js
@@ -4,20 +4,18 @@ const input = fs.readFileSync(0, 'utf-8').trim().split("\n");
 
 const target = +input.shift();
 	
-  let end = 1;
-  let sum = 1;
-  let count = 0;
+let end = 1;
+let sum = 1;
+let count = 0;
  
-  for (let start = 1; start <= target; start++) {
+for (let start = 1; start <= target; start++) {
     while (sum < target && end < target) {
-      end += 1;
-      sum += end;
+        end += 1;
+        sum += end;
     }
- 
     if (sum === target) {
-      count += 1;
+        count += 1;
     }
- 
     sum -= start;
-  }
-  console.log(count);
+}
+console.log(count);
