79d6dd48679de35300ef6ec436d65681f77d1524
노현주 <38433755+nohj0518@users.noreply.github.com>
2024-09-12 22:51:37 +0900

    [Bronze V] Title: 문자와 문자열, Time: 140 ms, Memory: 9448 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.js
@@ -0,0 +1,17 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+rl.on("line", (line) => {
+  input.push(line);
+});
+
+rl.on("close", () => {
+  const arr=input[0].split('');
+  console.log(arr[input[1]-1]);
+  process.exit();
+});
