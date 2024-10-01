f3221b67216b1ed18bb3f1207a62ff1c2ece0a82
노현주 <38433755+nohj0518@users.noreply.github.com>
2024-10-01 22:53:54 +0900

    [Gold V] Title: 수 고르기, Time: 220 ms, Memory: 23040 KB -BaekjoonHub

--- 백준/Gold/2230. 수 고르기/수 고르기.js
+++ 백준/Gold/2230. 수 고르기/수 고르기.js
@@ -1,29 +1,19 @@
 const fs = require("fs");
 
-const input = fs.readFileSync("/dev/stdin")
-.toString()
-.trim()
-.split("\n");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
 const [N, M] = input.shift().split(" ").map(Number);
 const arr = input.map(Number).sort((a, b) => a - b);
 
 let minDiff = Infinity;
-arr.map((X, idx) => {
-  let st = idx + 1;
-  let ed = N;
-  let target = X + M;
-  while (st < ed) {
-    let mid = Math.floor((st + ed) / 2);
-    if (arr[mid] >= target) {
-      ed = mid;
-    } else {
-      st = mid + 1;
-    }
+let en = 0;
+for (let st = 0; st < arr.length; st++) {
+  while (en < arr.length && arr[en] - arr[st] < M) {
+    en++;
   }
-  if (st < N) {
-    minDiff = Math.min(minDiff, arr[st] - X);
-  }
-});
+  if (en === N) break;
+  minDiff = Math.min(minDiff, arr[en] - arr[st]);
+}
 
 console.log(minDiff);
+
