4d094360b46b693c3422b876c89eff357cfbafa9
노현주 <38433755+nohj0518@users.noreply.github.com>
2025-07-12 22:42:41 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.14 ms, Memory: 32.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
@@ -0,0 +1,19 @@
+function solution(lottos, win_nums) {
+    // 1-45 중 6개를 찍어서 맞춰야 win
+    // 1등 6개 - 2등 5개 - 3등 4개 - 4등 3개 - 5등 2개
+    // 낙서 때문에 알아볼 수 없는 번호 0
+    const getWinner = (n) =>{
+        switch(n){
+            case 6: return 1;
+            case 5: return 2;
+            case 4: return 3;
+            case 3: return 4;
+            case 2: return 5;
+            default: return 6;
+        }
+    }
+    const canSelect = new Array(45).fill().map((_,i) => i+1).filter((t) => !lottos.includes(t))
+    const min = lottos.filter((t) => win_nums.includes(t)).length
+    const nope = lottos.filter((l) => l == 0).length
+    return [getWinner(min+nope), getWinner(min)];
+}
